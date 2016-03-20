5921376a57ed5e887d8d4e54b7a954f7b06c1e66
omkarvijay5 <omkarvijay5@gmail.com>
2016-03-20 23:27:50 +0530

    updated blog urls

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+"""blog URL Configuration
+
+The `urlpatterns` list routes URLs to blog views.
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+]
