d8463c481d6219d510943bc323f8b903c24f3c59
omkarvijay5 <omkarvijay5@gmail.com>
2016-03-20 23:35:03 +0530

    blog model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Blog(models.Model):
+    """
+    Each user can create his own blog through admin.
+    User can create only one blog of their own
+    """
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    description = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, unique=True)
+
+    def __unicode__(self):
+        return self.title
