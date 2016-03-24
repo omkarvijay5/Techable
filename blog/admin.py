d2976630dd4e57a27b440959fa89c4f6712b6185
omkarvijay5 <omkarvijay5@gmail.com>
2016-03-24 09:49:17 +0530

    pep8 fixes

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from blog.models import Blog, Category, Post
 
-from django_markdown.admin import MarkdownModelAdmin
 from django_markdown.models import MarkdownField
 from django_markdown.widgets import AdminMarkdownWidget
 
@@ -9,13 +8,16 @@ from django_markdown.widgets import AdminMarkdownWidget
 class BaseAdminMixin(object):
     exclude = ('slug',)
 
+
 @admin.register(Blog)
 class BlogAdmin(BaseAdminMixin, admin.ModelAdmin):
     pass
 
+
 @admin.register(Category)
 class CategoryAdmin(BaseAdminMixin, admin.ModelAdmin):
     pass
+
 
 @admin.register(Post)
 class PostAdmin(BaseAdminMixin, admin.ModelAdmin):
