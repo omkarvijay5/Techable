db34bc4af56634b374922db6e2ed84375c77c90a
omkarvijay5 <omkarvijay5@gmail.com>
2016-06-26 16:37:40 +0530

    Category admin and inline for Post

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,7 @@ from django.forms import ValidationError, ModelForm
 from django_markdown.models import MarkdownField
 from django_markdown.widgets import AdminMarkdownWidget
 
-from blog.models import Blog, Post, Image
+from blog.models import Blog, Post, Image, Category
 
 
 class BaseAdminMixin(object):
@@ -26,11 +26,23 @@ class BlogAdminForm(ModelForm):
 class BlogAdmin(BaseAdminMixin, admin.ModelAdmin):
     form = BlogAdminForm
 
+
 @admin.register(Image)
 class ImageAdmin(BaseAdminMixin, admin.ModelAdmin):
     pass
 
 
+class CategoryInlineAdmin(admin.TabularInline):
+    model = Category.posts.through
+    extra = 1
+
+
 @admin.register(Post)
 class PostAdmin(BaseAdminMixin, admin.ModelAdmin):
+    inlines = [CategoryInlineAdmin]
     formfield_overrides = {MarkdownField: {'widget': AdminMarkdownWidget}}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
