e854b58931aacff17e750b835f722cc40565fed0
omkarvijay5 <omkarvijay5@gmail.com>
2016-07-03 23:08:37 +0530

    post_detail view, url and basic template

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,12 @@ The `urlpatterns` list routes URLs to blog views.
 """
 from django.conf.urls import url, patterns
 
-from blog.views import dashboard
+from blog.views import dashboard, post_detail
 
 urlpatterns = patterns(
     '',
     url(r'^$', dashboard, name='dashboard'),
+
+    # post detail
+    url(r'^post-detail/(?P<post_slug>[\w-]+)/$', post_detail, name='post_detail'),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
+from blog.models import Post
 
 
 def dashboard(request):
     return render(request, 'blog/dashboard.html', {})
+
+def post_detail(request, post_slug):
+    post = get_object_or_404(Post, slug=post_slug)
+    context = {'post': post}
+    return render(request, 'blog/post_detail.html', context)
