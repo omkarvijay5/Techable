abb0834c36540cfd5f2cf010e382561b77fdb1ca
omkarvijay5 <omkarvijay5@gmail.com>
2016-07-04 12:08:48 +0530

    tags categories from database

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,24 @@
+from collections import defaultdict
+
 from django.shortcuts import render, get_object_or_404
 
 
 # Create your views here.
-from blog.models import Post
+from blog.models import Post, Category
 
 
 def dashboard(request):
     return render(request, 'blog/dashboard.html', {})
 
+
 def post_detail(request, post_slug):
-    post = get_object_or_404(Post, slug=post_slug)
-    context = {'post': post}
+    post = get_object_or_404(Post.objects.select_related('author'), slug=post_slug)
+    categories = Category.objects.all().prefetch_related('posts')
+    posts = Post.objects.filter(category__in=categories).select_related('author')
+
+    # calculate all tags for all posts
+    tags = []
+    for post in posts:
+        tags.extend(post.tags)
+    context = {'post': post, 'categories': categories, 'tags': tags, 'posts': posts}
     return render(request, 'blog/post_detail.html', context)
