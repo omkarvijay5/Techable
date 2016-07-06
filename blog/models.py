90f9cc233b3829d07b2928d1971a1369144dda4f
omkarvijay5 <omkarvijay5@gmail.com>
2016-07-06 11:40:31 +0530

    markdown_to_html property

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.conf import settings
 from django.core.exceptions import PermissionDenied
 from django.contrib.postgres.fields import ArrayField
-import markdown
+from markdown import markdown
 from django_markdown.models import MarkdownField
 
 from blog_utils.slugify import unique_slugify
@@ -33,19 +33,6 @@ class Blog(models.Model):
         super(Blog, self).save(*args, **kwargs)
 
 
-def markdown_to_html(markdownText, images):
-    image_ref = ""
-
-    for image in images:
-        image_url = settings.MEDIA_URL + image.image.url
-        image_ref = "%s\n[%s]: %s" % (image_ref, image, image_url)
-
-    md = "%s\n%s" % (markdownText, image_ref)
-    html = markdown.markdown(md)
-
-    return html
-
-
 class Image(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to="post_images")
@@ -71,11 +58,20 @@ class Post(models.Model):
     body = MarkdownField()
     images = models.ManyToManyField(Image, blank=True)
 
+    @property
+    def markdown_to_html(self):
+        image_ref = ""
+
+        for image in self.images.all():
+            image_url = settings.MEDIA_URL + image.image.url
+            image_ref = "%s\n[%s]: %s" % (image_ref, image, image_url)
+
+        md = "%s\n%s" % (self.body, image_ref)
+        html = markdown(md)
+        return html
+
     def __unicode__(self):
         return self.title
-
-    def body_html(self):
-        return markdown_to_html(self.body, self.images.all())
 
     def save(self, *args, **kwargs):
         if self.id is None:
