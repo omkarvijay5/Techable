fb5aeab4a8c790e97e2dec54f2ed2d775d70aca3
omkarvijay5 <omkarvijay5@gmail.com>
2016-08-14 08:02:15 +0530

    fix static image url

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -11,5 +11,5 @@ def get_meta_img_url(post):
     current_site = Site.objects.get_current()
     domain_name = current_site.domain
     if post_images:
-        return 'http://' + domain_name + post_images[0].image.url
+        return post_images[0].image.url
     return 'http://' + domain_name + settings.STATIC_URL + "/images/slider-wallpaper-colorlib-750x410.png"
