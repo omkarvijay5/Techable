f9128da7fa4c78e1b85f0502fbe0fc60c2b44939
omkarvijay5 <omkarvijay5@gmail.com>
2016-08-16 12:12:20 +0530

    fixed default image url

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -2,7 +2,6 @@ import re
 
 from django import template
 from django.conf import settings
-from django.contrib.sites.models import Site
 
 
 register = template.Library()
@@ -10,11 +9,9 @@ register = template.Library()
 @register.assignment_tag
 def get_meta_img_url(post):
     post_images = post.images.all()
-    current_site = Site.objects.get_current()
-    domain_name = current_site.domain
     if post_images:
         return post_images[0].image.url
-    return 'http://' + domain_name + settings.STATIC_URL + "/images/slider-wallpaper-colorlib-750x410.png"
+    return settings.STATIC_URL + "images/slider-wallpaper-colorlib-750x410.png"
 
 @register.filter
 def meta_desc(post):
