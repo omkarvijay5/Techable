9c444acec9aecfe821992b19091b42f2e9ab5a18
omkarvijay5 <omkarvijay5@gmail.com>
2016-08-21 08:51:34 +0530

    rename templatetags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -7,12 +7,12 @@ from django.conf import settings
 register = template.Library()
 
 @register.assignment_tag
-def get_meta_img_url(post):
+def get_post_img_url(post):
     post_images = post.images.all()
     if post_images:
         return post_images[0].image.url
     return settings.STATIC_URL + "images/slider-wallpaper-colorlib-750x410.png"
 
 @register.filter
-def meta_desc(post):
-    return re.sub("(<[^>]+>)", '', post.markdown_to_html)[:100]
+def post_desc(post):
+    return re.sub("(<[^>]+>)", '', post.markdown_to_html)[:400]
